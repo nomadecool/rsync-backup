1c1f6bb26db561ccda16ab333085e0f1e3ea2f80
nomadecool <raul@lajous.es>
2023-08-01 18:00:22 +0200

    20230801 18:00 arrancamos todo de nuevo

--- entryPoint.sh
+++ entryPoint.sh
@@ -8,89 +8,66 @@ elif [[ "${NODE,,}" == "sender" ]]; then
 elif [[ "${NODE,,}" == "receiver" ]]; then
     fail="0"
 elif ! grep -q "/backup" /etc/mtab; then
-    echo "Please define a /backup config path."
-    echo "Defina una ruta de configuración /backup."
-    echo "This should be a physical path, such as:"
-    echo "Esto debería ser una ruta física, como:"
-    echo "-v \"~/backup:/backup\""
+    echo "Defina una ruta de configuración /backup - Please define a /backup config path."
+    echo "Esto debería ser una ruta física, como: - This should be a physical path, such as:"
+    echo "-v \\"~/backup:/backup\\""
     fail="1"
 fi
 if [[ "${fail}" -eq "1" ]]; then
-    echo "Please set environmental flag 'NODE=[sender|receiver]'"
     echo "Por favor, establezca la bandera de ambiente 'NODE=[sender|receiver]'"
+    echo "Please set environmental flag 'NODE=[sender|receiver]'"
     exit 1
 fi
 
-# Create rsync-user if it does not exist
-if ! id -u rsync-user > /dev/null 2>&1; then
-    adduser --gecos "" --disabled-password rsync-user
-    # We can't SSH into the rsync-user if it doesn't have a password set
-    # Set a random 36 character string as the password
-    rsyncuserPass="$(date +%s | sha256sum | base64 | head -c 36)"
-    echo "rsync-user:${rsyncuserPass}" | chpasswd
-    mkdir -p /home/rsync-user/.ssh
-    chown rsync-user:rsync-user /home/rsync-user/.ssh
-    chmod 700 /home/rsync-user/.ssh
-fi
-
 if [[ "${NODE,,}" == "sender" ]]; then
-    if ! grep -q "/home/rsync-user" /etc/mtab; then
-        echo "Please define a rsync-user config path."
-        echo "Defina una ruta de configuración rsync-user."
-        echo "This should be a physical path, such as:"
-        echo "Esto debería ser una ruta física, como:"
-        echo "-v \"~/Docker/rsync-backup/rsync-user:/home/rsync-user\""
+    if ! grep -q "/home/root" /etc/mtab; then
+        echo "Por favor defina una ruta de configuración para root - Please define a root config path."
+        echo "Esto debería ser una ruta física, como: - This should be a physical path, such as:"
+        echo "-v \\"~/Docker/rsync-backup/root:/home/root\\""
         exit 2
     fi
-    if ! [[ -e "/home/rsync-user/.ssh/id_ed25519" ]]; then
-        su - rsync-user -c "ssh-keygen -b 2048 -t ed25519 -f /home/rsync-user/.ssh/id_ed25519 -q -N ''"
-        chmod 400 "/home/rsync-user/.ssh/id_ed25519"
-        chown rsync-user:rsync-user "/home/rsync-user/.ssh/id_ed25519"
+    if ! [[ -e "/home/root/.ssh/id_ed25519" ]]; then
+        ssh-keygen -b 2048 -t ed25519 -f /home/root/.ssh/id_ed25519 -q -N ''
+        chmod 400 "/home/root/.ssh/id_ed25519"
+        chown root:root "/home/root/.ssh/id_ed25519"
         echo ""
-        echo "On the receiver node, create an authorized_keys file in the config directory"
-        echo "En el nodo receptor, cree un archivo authorized_keys en el directorio de configuración"
+        echo "En el nodo receptor, crea un archivo authorized_keys en el directorio de configuración - On the receiver node, create an authorized_keys file in the config directory"
         echo ""
-        echo "For example, if your 'config' volume mount on the receiver is:"
-        echo "Por ejemplo, si su montaje de volumen 'config' en el receptor es:"
-        echo "-v /Docker/rsync-backup/rsync-user:/home/rsync-user"
+        echo "Por ejemplo, si su montaje de volumen 'config' en el receptor es / For example, if your 'config' volume mount on the receiver is:"
+        echo "-v /Docker/rsync-backup/root:/home/root"
         echo ""
-        echo "Then you would create a file at:"
-        echo "Entonces crearías un archivo en:"
-        echo "/Docker/rsync-backup/rsync-user/.ssh/authorized_keys"
+        echo "Entonces crearías un archivo en / Then you would create a file at:"
+        echo "/Docker/rsync-backup/root/.ssh/authorized_keys"
         echo ""
-        echo "Copy/paste the contents between the ##### markers into that authorized_keys file:"
-        echo "Copie/pegue el contenido entre los marcadores ##### en ese archivo authorized_keys:"
+        echo "Copie/pegue el contenido entre los marcadores ##### en ese archivo authorized_keys / Copy/paste the contents between the ##### markers into that authorized_keys file:"
         echo ""
         echo "####### COPY BELOW THIS LINE, BUT NOT THIS LINE ######## ####### COPIE DEBAJO DE ESTA LÍNEA, PERO NO ESTA LÍNEA ########"
-        cat "/home/rsync-user/.ssh/id_ed25519.pub"
+        cat "/home/root/.ssh/id_ed25519.pub"
         echo "####### COPY ABOVE THIS LINE, BUT NOT THIS LINE ######## ####### COPIE ARRIBA DE ESTA LÍNEA, PERO NO ESTA LÍNEA ########"
         echo ""
-        echo "Once done, re-start this container."
-        echo "Una vez hecho esto, reinicie este contenedor."
+        echo "Una vez hecho esto, reinicie este contenedor / Once done, re-start this container."
         exit 0
     fi
 
-    if ! [[ -e "/home/rsync-user/.ssh/known_hosts" ]]; then
-        touch /home/rsync-user/.ssh/known_hosts
-        chown rsync-user:rsync-user /home/rsync-user/.ssh
-        chown rsync-user:rsync-user /home/rsync-user/.ssh/known_hosts
-        chmod 700 /home/rsync-user/.ssh
-        chmod 600 /home/rsync-user/.ssh/known_hosts
-        su rsync-user -c "ssh-keyscan -p ${REM_SSH_PORT} -t rsa ${REM_HOST} >> /home/rsync-user/.ssh/known_hosts 2>/dev/null"
-        if [[ "${?}" -ne "0" || "$(wc -l "/home/rsync-user/.ssh/known_hosts" | awk '{print $1}')" -eq "0" ]]; then
-            echo "Unable to initiate keyscan. Is the receiver online?"
-            echo "Incapaz de iniciar el keyscan. ¿Está en línea el receptor?"
-            rm "/home/rsync-user/.ssh/known_hosts"
+    if ! [[ -e "/home/root/.ssh/known_hosts" ]]; then
+        touch /home/root/.ssh/known_hosts
+        chown root:root /home/root/.ssh
+        chown root:root /home/root/.ssh/known_hosts
+        chmod 700 /home/root/.ssh
+        chmod 600 /home/root/.ssh/known_hosts
+        su root -c "ssh-keyscan -p ${REM_SSH_PORT} -t rsa ${REM_HOST} >> /home/root/.ssh/known_hosts 2>/dev/null"
+        if [[ "${?}" -ne "0" || "$(wc -l "/home/root/.ssh/known_hosts" | awk '{print $1}')" -eq "0" ]]; then
+            echo "Incapaz de iniciar el keyscan. ¿Está en línea el receptor? / Unable to initiate keyscan. Is the receiver online?"
+            rm "/home/root/.ssh/known_hosts"
             exit 3
         fi
     fi
 
     inotifywait -r -m -e close_write --format '%w%f' /backup/ | while read MODFILE
     do
-        rsync -a -P -e "ssh -p ${REM_SSH_PORT} -o StrictHostKeyChecking=no" /backup/ rsync-user@${REM_HOST}:/backup/ --delete
+        rsync -a -P -e "ssh -p ${REM_SSH_PORT}" /backup/ root@${REM_HOST}:/backup/ --delete
         if [[ "${?}" -ne "0" ]]; then
-            echo "Unable to initiate backup rsync. Is the receiver online?"
-            echo "Incapaz de iniciar el rsync de respaldo. ¿Está en línea el receptor?"
+            echo "Incapaz de iniciar el rsync de respaldo. ¿Está en línea el receptor? / Unable to initiate backup rsync. Is the receiver online?"
             exit 4
         fi
     done
@@ -98,26 +75,21 @@ fi
 
 if [[ "${NODE,,}" == "receiver" ]]; then
     if ! grep -q "/etc/ssh" /etc/mtab; then
-        echo "Please define an /etc/ssh config path."
         echo "Defina una ruta de configuración /etc/ssh."
-        echo "This should be a physical path, such as:"
-        echo "Esto debería ser una ruta física, como:"
-        echo "-v \"~/Docker/rsync-backup/etc-ssh:/etc/ssh\""
+        echo "Please define an /etc/ssh config path."
+        echo "Esto debería ser una ruta física, como / This should be a physical path, such as:"
+        echo "-v \\"~/Docker/rsync-backup/etc-ssh:/etc/ssh\\""
         exit 5
     fi
-    if ! grep -q "/home/rsync-user" /etc/mtab; then
-        echo "Please define a rsync-user config path."
-        echo "Defina una ruta de configuración rsync-user."
-        echo "This should be a physical path, such as:"
-        echo "Esto debería ser una ruta física, como:"
-        echo "-v \"~/Docker/rsync-backup/rsync-user:/home/rsync-user\""
+    if ! grep -q "/home/root" /etc/mtab; then
+        echo "Por favor defina una ruta de configuración para root / Please define a root config path."
+        echo "Esto debería ser una ruta física, como / This should be a physical path, such as:"
+        echo "-v \\"~/Docker/rsync-backup/root:/home/root\\""
         exit 6
     fi
-    if ! [[ -e "/home/rsync-user/.ssh/authorized_keys" ]]; then
-        echo "Please obtain the 'authorized_keys' file from the sender,"
-        echo "Por favor, obtenga el archivo 'authorized_keys' del remitente,"
-        echo "and add it at your home/rsync-user/.ssh/authorized_keys path"
-        echo "y añádalo en su ruta home/rsync-user/.ssh/authorized_keys"
+    if ! [[ -e "/home/root/.ssh/authorized_keys" ]]; then
+        echo "Por favor, obtenga el archivo 'authorized_keys' del remitente / Please obtain the 'authorized_keys' file from the sender,"
+        echo "y añádalo en su ruta home/root/.ssh/authorized_keys / and add it at your home/root/.ssh/authorized_keys path"
         exit 7
     fi
     sshKeyArr=("ssh_host_dsa_key" "ssh_host_dsa_key.pub" "ssh_host_ecdsa_key" "ssh_host_ecdsa_key.pub" "ssh_host_ed25519_key" "ssh_host_ed25519_key.pub" "ssh_host_rsa_key" "ssh_host_rsa_key.pub")
@@ -128,12 +100,14 @@ if [[ "${NODE,,}" == "receiver" ]]; then
     done
     mkdir -p /run/sshd
     mv /sshd_config /etc/ssh/sshd_config
-    # Ensure permissions are correct on the rsync-user directory, or it won't let
-    # us rsync/ssh in as the rsync-user
-    # Asegúrese de que los permisos sean correctos en el directorio rsync-user, o no nos permitirá
-    # rsync/ssh como rsync-user
-    chmod 700 /home/rsync-user
-    chmod 700 /home/rsync-user/.ssh
-    chmod 600 /home/rsync-user/.ssh/authorized_keys
-    exec /usr/sbin/sshd -D -e
+    # No podemos hacer SSH en root si no se establece una contraseña / We can't SSH into the root if it doesn't have a password set
+    # Establecer una cadena aleatoria de 36 caracteres como contraseña / Set a random 36 character string as the password
+    rootPass="$(date +%s | sha256sum | base64 | head -c 36)"
+    echo "root:${rootPass}" | chpasswd
+    # Asegúrese de que los permisos sean correctos en el directorio root, o no nos permitirá / Ensure permissions are correct on the root directory, or it won't let
+    # rsync/ssh como root - us rsync/ssh in as the root user
+    chmod 700 /root
+    chmod 700 /root/.ssh
+    chmod 600 /root/.ssh/authorized_keys
+    /usr/sbin/sshd -D -e
 fi
